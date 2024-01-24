7df8290021e21d4efc54cea284a473c039a89d0a
Adam Zajler <adamzajler01@gmail.com>
2024-01-24 18:19:55 +0100

    Added lint repairs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import {TODO} from '../../components/TODO';
-import './globals.css'
+import { TODO } from '../../components/TODO';
+import './globals.css';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <TODO />
+      <TODO />
     </main>
   );
 }
